e1f4460224cd37a500ac1c918d585e402738bbc5
Mohamed Reda Ali <m07hamedreda25@gmail.com>
2026-01-06 01:21:39 +0200

    CTA Button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { ArrowRight } from "lucide-react"
 
 const Hero = () => {
   return <section className="relative min-h-screen flex flex-col justify-center pt-20 overflow-hidden text-white bg-gradient-to-tr from-zinc-950 via-blue-700 to-indigo-300">
-    {/* Gain Texture */}
+    {/* Grain Texture */}
     <div style={{
         backgroundImage: "url(/images/grain.svg)",
         backgroundSize: "cover",
@@ -21,21 +21,28 @@ const Hero = () => {
                 Design studio that not only creates digital products but also experiences.
             </h2>
             {/* CTA Button */}
-            <button className="mt-12 p-3 px-6 flex items-center gap-4 tracking-tight cursor-pointer rounded-full bg-prime-accent text-zinc-900 font-semibold transition-all duration-300 hover:scale-105">
+            <button className="mt-12 p-3 px-6 flex items-center gap-4 tracking-tight cursor-pointer rounded-full bg-gray-400 text-white/90 font-semibold transition-all duration-300 hover:scale-105">
                 let's Collaborate 
-                <div className="size-10 bg-zinc-900 rounded-full center-item">
-                    <ArrowRight className="text-prime-accent" size={20} />
-                </div>
+                {/* <div className="size-10 bg-zinc-900 rounded-full center-item"> */}
+                    <ArrowRight className="text-white/90" size={20} />
+                {/* </div> */}
             </button>
         </div>
     </div>
-    {/* Hugee Brand-Text */}
+    {/* Huge Brand-Text */}
     <div className="relative w-full">
         <h1 className="absolute text-white text-[100px] tracking-tighter font-bold -rotate-90 bottom-2 2xl:-bottom-60 -right-42 2xl:left-[120px] lg:rotate-0 lg:left-[60px] lg:-bottom-32 lg:text-[20vw] leading-none select-none opacity-100">
             Boulevard
         </h1>
-        <div className="absolute right-14 -top-40 text-[10px] uppercase font-bold tracking-[0.2rem] opacity-60 hidden lg:block">
-            (Scroll for more)
+        
+        {/* Scroll Indicator */}
+        <div className="absolute right-14 -top-40 flex flex-col items-center gap-4 opacity-70 lg:flex">
+            <div className="text-[10px] uppercase font-bold tracking-[0.25rem] text-white/80">
+                Scroll
+            </div>
+            <div className="relative w-5 h-10 border-2 border-white/40 rounded-full flex justify-center">
+                <div className="w-1 h-2 bg-white rounded-full mt-2 animate-bounce" />
+            </div>
         </div>
     </div>
   </section>
